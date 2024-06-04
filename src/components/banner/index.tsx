cd8a5f2736c794c05e64b5aa2298f40b008a4b79
Renan <renan.fachin@hotmail.com>
2024-06-04 16:48:17 -0300

    feat: Cria componente de banner

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,8 +1,23 @@
-import '../../styles/pages/Home/home.sass'
+import '../../styles/pages/Home/banner.sass'
+
+import { Link } from 'react-router-dom'
+
 export function Banner() {
   return (
-    <div>
+    <div id="banner-container">
       <img src="/banner.png" alt="" className="banner" />
+
+      <div id="details">
+        <div>
+          <h2>Venha conhecer nossas promoções</h2>
+
+          <p>50% Off nos produtos</p>
+        </div>
+
+        <Link to={'/'} className="see-product-button">
+          Ver produto
+        </Link>
+      </div>
     </div>
   )
 }
